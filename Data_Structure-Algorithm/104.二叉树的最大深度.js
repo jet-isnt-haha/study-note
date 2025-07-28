665e6d1b8c29c8af43d21a5568f2156f57e822b6
jet-isnt-haha <1802966308@qq.com>
2025-07-28 12:54:11 +0800

    2025-07-28

---
+++ Data_Structure-Algorithm/104.二叉树的最大深度.js
@@ -0,0 +1,20 @@
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    if (root === null) {
+        return 0;
+    } else {
+        let left = maxDepth(root.left);
+        let right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+};
