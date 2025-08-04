4081e77b14066e30258fefc96bc6083cbe1cd0f3
jet-isnt-haha <1802966308@qq.com>
2025-08-04 13:39:34 +0800

    2025-08-04(2)

---
+++ JS-手写/14-图片懒加载.js
@@ -0,0 +1,25 @@
+let imgList = [...document.querySelectorAll('img')];
+let length = imgList.length;
+
+const imgLazyLoad = function () {
+    let count = 0;
+
+    return (function () {
+        let deleteIndexList = [];
+        imgList.forEach((img, index) => {
+            let rec = img.getBoundingClientRect();
+            if (rec.top < window.innerHeight) {
+                img.src = img.dataset.src;
+                deleteIndexList.push(index);
+                count++;
+                if (count === length) {
+                    document.removeEventListener('scroll', imgLazyLoad);
+                }
+            }
+        })
+        imgList = imgList.filter((img, index) => !deleteIndexList.includes(index))
+    })()
+
+}
+
+document.addEventListener('scroll', imgLazyLoad);
---
+++ JS-手写/15-防抖.js
@@ -0,0 +1,52 @@
+//! 防抖：触发高频事件N秒后只会执行一次，如果N秒内事件再次触发，则会重新计时。
+
+//  简单版
+//函数内部支持使用this和event事件
+function debounce(fn, wait) {
+    let timeout;
+    return function () {
+        let context = this;
+        let args = arguments;
+        clearTimeout(timeout);
+        console.log('Recalculate');
+        timeout = setTimeout(() => fn.apply(context, args), wait);
+    }
+}
+
+//最终版
+//还支持：立即执行、函数可能有返回值、支持取消功能
+
+function debounceProMax(fn, wait, immediate) {
+    let timeout, result;
+
+    let debounceProMax = function () {
+        let context = this;
+        let args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            let callNow = !timeout;
+            console.log('Recalculate');
+            timeout = setTimeout(() => {
+                timeout = null; if (!callNow) fn.apply(context, args)
+            }, wait);
+
+            if (callNow) {
+                result = fn.apply(context, args)
+                console.log('execute success');
+            };
+        } else {
+            timeout = setTimeout(() => {
+                fn.apply(context, args);
+            }, wait);
+        }
+        return result;
+    }
+
+    debounceProMax.cancel = function () {
+        clearTimeout(timeout);
+        timeout = null;
+
+    }
+    return debounceProMax;
+}
