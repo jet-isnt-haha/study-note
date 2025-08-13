27d5d4f278e32df20837f519007d952a08ef9622
jet-isnt-haha <1802966308@qq.com>
2025-08-13 12:06:28 +0800

    2025-08-13(2)

---
+++ JS-手写/26-apply.js
@@ -0,0 +1,20 @@
+Function.prototype.myApply = function (context, arr) {
+    context = context || globalThis;
+
+    context.fn = this;
+
+    let result;
+    if (!arr) {
+        result = context.fn();
+    } else {
+
+        const args = [];
+        for (let i = 1, len = arr.length; i < len; ++i) {
+            args.push(`arr[${i}]`);
+        }
+
+        result = eval(`context.fn(${args})`);
+    }
+    delete context.fn;
+    return result;
+}
