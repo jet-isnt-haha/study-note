db1e390dbb32c9810d15d8ba9680139b1300adfa
jet-isnt-haha <1802966308@qq.com>
2025-09-25 23:24:44 +0800

    2025-09-25(3)

---
+++ Typescript/index.ts
@@ -0,0 +1,16 @@
+let str: string = "jet";
+
+console.log(str);
+
+let n: null = null;
+
+//严格模式下不能将null赋值给类型void
+//let v1: void = null;
+
+let v2: void = undefined;
+
+let a: undefined = undefined;
+
+//严格模式下不允许这两个类型相互赋值
+/* n=a;
+a=n; */
