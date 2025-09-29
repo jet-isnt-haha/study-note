5d0d9f01d5cd29bdedda68a14ff623542bf198b7
jet-isnt-haha <1802966308@qq.com>
2025-09-29 13:34:02 +0800

    2025-09-29

---
+++ Data_Structure-Algorithm/155.最小栈.js
@@ -0,0 +1,42 @@
+
+var MinStack = function () {
+    this.stack = [[0, Infinity]];//栈底哨兵
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+    this.stack.push([val, Math.min(this.getMin(), val)])
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1][0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.stack[this.stack.length - 1][1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
