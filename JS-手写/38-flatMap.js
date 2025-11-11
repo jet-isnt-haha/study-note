d727a0295badb3eabf9c084745d879284388f32c
jet-isnt-haha <1802966308@qq.com>
2025-11-11 08:33:32 +0800

    2025-11-11(2)

--- JS-手写/38-flatMap.js
+++ JS-手写/38-flatMap.js
@@ -1,22 +1,31 @@
 
-function flatMap(arr = []) {
-    let list = [];
-    arr.forEach(item => {
+//! flatMap 是先map再flat
+console.log([1, 2, [3], 4].flatMap((x) => x + 1));//[2,3,31,4]
+console.log([2] + 1);//21
 
-        if (Array.isArray(item)) {
-            const l = flatMap(item);
-            list.push(...l);
-        } else {
-            list.push(item);
+Array.prototype.flatMap = function (callback, thisArg) {
+    if (this == null) throw new TypeError('Array.prototype.flatMap call on null or undefined');
+    if (typeof callback !== 'function') throw new TypeError(callback, ' is not a function');
+
+    const O = Object(this);
+    const len = O.lenght >>> 0;
+    const result = [];
+
+    for (let i = 0; i < len; ++i) {
+        if (!(i in O)) {
+            //保留稀疏数组的空位：在规范中map会跳过hole，flatMap也会跳过
         }
-    })
-
-    return list;
+        const mapped = callback.call(thisArg, O[i], i, O);
+        //将返回值展平一层，如果是数组就concat
+        if (Array.isArray(mapped)) {
+            result.push(...mapped);
+        } else {
+            result.push(mapped);
+        }
+    }
+    return result;
 }
-let myFlatMap = function (fn) {
-    let target = this;
-    return target.map((i) => fn(i)).flat();
-};
+
 Array.prototype.myFlatMap = myFlatMap;
 let arr = ["it's Sunny in", "", "California"];
 let arr1 = arr.map((x) => x.split(" "));
