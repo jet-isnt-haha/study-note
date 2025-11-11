61208917968e816506da1f496f3aeb8f19abe32d
jet-isnt-haha <1802966308@qq.com>
2025-11-11 08:14:36 +0800

    2025-11-11

--- Data_Structure-Algorithm/279.完全平方数.js
+++ Data_Structure-Algorithm/279.完全平方数.js
@@ -13,3 +13,26 @@ var numSquares = function (n) {
     }
     return dp[n];
 };
+
+{
+    var numSquares = function (n) {
+        let i = 0;
+        for (; (i + 1) * (i + 1) <= n; ++i);
+
+        let numSquares = [];
+        for (let x = 1; x <= i; ++x) {
+            numSquares.push(x * x);
+        }
+
+        const dp = new Array(n + 1).fill(Infinity);
+        dp[0] = 0;
+
+        for (let i = 0; i < numSquares.length; ++i) {
+            for (let j = numSquares[i]; j <= n; ++j) {
+                dp[j] = Math.min(dp[j - numSquares[i]] + 1, dp[j]);
+            }
+        }
+        return dp[n];
+    };
+
+}
